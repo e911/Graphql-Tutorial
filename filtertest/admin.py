e0020d1a08d2c9b750695cdc66c0fae7be75539c
t_barun <pujanthapa88.63@gmail.com>
2018-01-21 12:39:02 +0545

    Admin registered for filtertest

--- filtertest/admin.py
+++ filtertest/admin.py
@@ -1,17 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-# from filtertest.models import
-#
-#
-# class FourthAdmin(admin.ModelAdmin):
-#     class Meta:
-#         model = Category
-#
-#
-# class ThirdAdmin(admin.ModelAdmin):
-#     class Meta:
-#         model = Ingredient
-#
-# admin.site.register(Category, FourthgAdmin)
-# admin.site.register(Ingredient, ThirdAdmin)
+from filtertest.models import Animal
+
+
+class FourthAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Animal
+
+admin.site.register(Animal, FourthAdmin)
