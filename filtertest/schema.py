15846ed5151cc389525dbf881994dda2a83157d4
t_barun <pujanthapa88.63@gmail.com>
2018-01-21 11:34:27 +0545

    Custom FIlters for graphene

--- filtertest/schema.py
+++ filtertest/schema.py
@@ -1,3 +1,4 @@
+import django_filters
 from graphene import relay
 from graphene.types.objecttype import ObjectType
 from graphene_django.filter.fields import DjangoFilterConnectionField
@@ -15,6 +16,22 @@ class AnimalNode(DjangoObjectType):
         }
         interfaces = (relay.Node,)
 
+#custom filter
+class AnimalFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(field_name='name', lookup_expr='iexact')
+
+    class Meta:
+        model = Animal
+        fields = ['name', 'is_domesticated']
+
+    @property
+    def qs(self):
+        # The query context can be found in self.request.
+        return super(AnimalFilter, self).qs
+
 class Query(ObjectType):
     animal = relay.Node.Field(AnimalNode)
     all_animals = DjangoFilterConnectionField(AnimalNode)
+    #custom filter passed as argument
+    all_animals_customfilter = DjangoFilterConnectionField(AnimalNode,
+                                                           filterset_class=AnimalFilter)
